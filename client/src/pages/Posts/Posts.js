ee74644942e6c40c2877d36c9cb0672c2e9f9d9f
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-02-28 23:16:05 +1100

    feat: add posts page on client

--- client/src/pages/Posts/Posts.js
+++ client/src/pages/Posts/Posts.js
@@ -1,6 +1,55 @@
 import React from "react";
 import Post from "../../components/Post/Post";
+import {gql, useQuery} from '@apollo/client'
+import {Spinner} from "react-bootstrap";
+
+const GET_POSTS = gql`
+  query {
+    posts {
+      userErrors {
+        message
+      }
+      posts {
+        title
+        content
+        createdAt
+        user {
+          id
+          name
+        }
+      }
+    }
+  }
+
+`;
 
 export default function Posts() {
-  return <div></div>;
+
+  const {data, error, loading} = useQuery(GET_POSTS);
+
+  console.log({data, error, loading})
+
+  if (error) {
+    return <div>Error Page</div>
+  }
+
+  if (loading) {
+    return <div><Spinner  animation={"border"}/></div>
+  }
+
+  const {posts} = data;
+  return (
+    <div>
+      {
+        posts.posts.map(post => <Post
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          content={post.content}
+          date={post.createdAt}
+          user={post.user.name}
+        />)
+      }
+    </div>
+  );
 }
